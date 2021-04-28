ff53696527c846a54b9ca8cd21a5b2daa0b6106c
Soumyajit <soumyajit660@gmail.com>
2021-04-28 23:53:06 +0530

    :zap: Solve problems

---
+++ Peak Index in a Mountain Array/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        for i in range(1, len(arr)-1):
+            if arr[i-1] < arr[i] and arr[i+1] < arr[i]:
+                return i
+            
+        # return arr.index(max(arr)) -> One liner solution
