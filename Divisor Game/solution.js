a8ebd68429ac251f33d214299f2967da636128ff
Soumyajit <soumyajit660@gmail.com>
2021-05-06 20:21:02 +0530

    :zap: Solve problems

---
+++ Divisor Game/solution.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function (n) {
+    const result = (n % 2 === 0) ? true : false;
+    return result;
+};
---
+++ Divisor Game/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        result = True if n % 2 == 0 else False
+        return result
